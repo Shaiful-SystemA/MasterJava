7e4fcfa907c93d3f6b6954b18b66af41a59a0374
Shaiful-SystemA <e0001339@u.nus.edu>
2016-11-22 00:26:19 -0500

    Second Eclipse

--- FromCS/src/PowerTest.java
+++ FromCS/src/PowerTest.java
@@ -1008,6 +1008,23 @@ public boolean xyBalance(String str)
 	
 }
 
+// given two strings, a and b, create a bigger string made of the first chat of a, the first char of b,
+// the second char of a, the second char of b and so on. any leftover chars go at the end of the result
+
+//mixString("abc","xyz") "axbyxc"
+//mixString("Hi", "There") "Hi there"
+//mixString("xxxx","There") "xTxherxre"
+
+public String mixString(String a, String b){
+	String res ="";
+	for(int i = 0; i <Math.min(a.length(), b.length()); i++)
+		res += "" + a.charAt(i) + b.charAt(i);
+	if(a.length() > b.length())
+		return res + a.substring(b.length());
+	return res + b.substring(a.length());
+}
+
+
 // Given two strings, append them together and re
 
 	public static void main(String[] args) {
